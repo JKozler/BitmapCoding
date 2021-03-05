270a0d48d35ad03b3df056938de79c608fd8d976
Kozler Jakub <kozler.jakub.2017@skola.ssps.cz>
2021-03-05 13:40:38 +0100

    Add project files.

---
+++ BitmapCoding/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace BitmapCoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap bm = new Bitmap("guitar.png");
+        }
+    }
+}
