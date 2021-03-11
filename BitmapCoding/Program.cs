fed652c618e452b351b8ee578c6a9e25cc36a3ef
Kozler Jakub <kozler.jakub.2017@skola.ssps.cz>
2021-03-11 09:50:05 +0100

    Edit + add comments and regions

--- BitmapCoding/Program.cs
+++ BitmapCoding/Program.cs
@@ -51,30 +51,26 @@ namespace BitmapCoding
             }
             return newBmp;
         }
-
+        #region Zandání textu do Bitmapy
         public static Bitmap MergeText(string text, Bitmap bmp)
         {
             State s = State.HIDING;
-
             int charIndex = 0;
             int charValue = 0;
             long colorUnitIndex = 0;
-
             int zeros = 0;
-
             int R = 0, G = 0, B = 0;
-
+            //Získám výšku obrázku a s ní pracuji (na úrovni pixelů)
             for (int i = 0; i < bmp.Height; i++)
             {
+                //Získám šířku obrázku a s ní pracuji (na úrovni pixelů)
                 for (int j = 0; j < bmp.Width; j++)
                 {
                     Color pixel = bmp.GetPixel(j, i);
-
-                    pixel = Color.FromArgb(pixel.R - pixel.R % 2,
-                        pixel.G - pixel.G % 2, pixel.B - pixel.B % 2);
-
+                    pixel = Color.FromArgb(pixel.R - pixel.R % 2, pixel.G - pixel.G % 2, pixel.B - pixel.B % 2);
+                    //Nastavení RGB
                     R = pixel.R; G = pixel.G; B = pixel.B;
-
+                    //Projedu a nastavím nově PGB
                     for (int n = 0; n < 3; n++)
                     {
                         if (colorUnitIndex % 8 == 0)
@@ -85,10 +81,8 @@ namespace BitmapCoding
                                 {
                                     bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
                                 }
-
                                 return bmp;
                             }
-
                             if (charIndex >= text.Length)
                             {
                                 s = State.FILL_WITH_ZEROS;
@@ -146,14 +140,14 @@ namespace BitmapCoding
             }
             return bmp;
         }
-
+        #endregion
+        #region Získání textu
         public static string ExtractText(Bitmap bmp)
         {
             int colorUnitIndex = 0;
             int charValue = 0;
-
             string extractedText = String.Empty;
-
+            //To samé jako nahoře, akorát opačně
             for (int i = 0; i < bmp.Height; i++)
             {
                 for (int j = 0; j < bmp.Width; j++)
@@ -180,20 +174,15 @@ namespace BitmapCoding
                                 }
                                 break;
                         }
-
                         colorUnitIndex++;
-
                         if (colorUnitIndex % 8 == 0)
                         {
                             charValue = reverseBits(charValue);
-
                             if (charValue == 0)
                             {
                                 return extractedText;
                             }
-
                             char c = (char)charValue;
-
                             extractedText += c.ToString();
                         }
                     }
@@ -202,13 +191,13 @@ namespace BitmapCoding
 
             return extractedText;
         }
+        #endregion
         public static int reverseBits(int n)
         {
             int result = 0;
             for (int i = 0; i < 8; i++)
             {
                 result = result * 2 + n % 2;
-
                 n /= 2;
             }
             return result;
